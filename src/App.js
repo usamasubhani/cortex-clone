1ae20f079d19a50949f85567e0bdcadb99fa1fa1
Usama Subhani <reachusama@protonmail.com>
2020-07-27 22:51:19 +0500

    Implement Hero/Header section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useLayoutEffect, useRef } from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
     </div>
   );
 }
 
 export default App;
+
+
+function Hero() {
+
+  const brain = useRef(null)
+
+  useLayoutEffect(() => {
+    var brainFrames =   [
+      { transform: 'translateY(0%)' },
+      { transform: 'translateY(-3%)' },
+      { transform: 'translateY(0%)' },
+    ];
+
+    
+    var brainTiming = {
+      duration: 3600,
+      iterations: Infinity
+    };
+    
+    var brainAnim = brain.current.animate(
+    brainFrames, brainTiming);
+
+  }, 3000)
+  return (
+    <div>
+      <div className="hero">
+      
+        <div className="herotitle" >
+          <div className="title-1">CORTEX</div>
+          <div className="title-2">COPYWRITER</div>
+          <div className="desc">Enhance your communications with psychology-based copywriting and UX writing</div>
+          <button className="send-btn">Send a message</button>
+        </div>
+        <div className="img-container">
+          <img className="heroimg" src="https://www.cortexcopywriter.com/img/intro/Intro_Featured_Image_Empty.svg" />
+          {/* <img className="a" src="https://www.cortexcopywriter.com/img/intro/Intro_Front_Layer.svg" /> */}
+          <img className="brain" ref={brain} src="https://www.cortexcopywriter.com/img/intro/Intro_Brain.svg" />
+          
+        </div>
+        
+      </div>
+    </div>
+  )
+}
